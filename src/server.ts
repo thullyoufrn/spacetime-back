f84c2104d594b2819ee375a7e8c5a0d8ae1fdd26
Thullyo Damasceno <thullyodamasceno.3@gmail.com>
2023-05-19 21:18:02 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true, // all URLs of frontend can access this backend
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
